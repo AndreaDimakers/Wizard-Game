18ce39e4dc86d2073f591ff00edad1d036461139
AndreaDimakers <andrea.vazquez@grupodimakers.com>
2026-03-06 20:01:40 -0600

    changes to settings.py

--- gameapp/settings.py
+++ gameapp/settings.py
@@ -16,7 +16,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['idmdev.ddns.net']
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
@@ -114,7 +114,3 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-STATIC_URL = 'static/'
